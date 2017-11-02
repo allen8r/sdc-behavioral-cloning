b3193a5198a405966db6812df3c088b0cec88829
Allen C. Lau <allen.c.lau@gmail.com>
2017-11-01 19:54:21 -0700

    Augment image data by horizontally flipping the images

--- model.py
+++ model.py
@@ -21,8 +21,18 @@ for row in rows:
     steering_angle = float(row[3])
     steering_angles.append(steering_angle)
 
-X_train = np.array(images)
-y_train = np.array(steering_angles)
+# Augment data by horizontally flipping the images
+augmented_images = []
+augmented_steering_angles = []
+for image, sterring_angle in zip(images, steering_angles):
+    augmented_images.append(image)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_steering_angles.append(steering_angle)
+    augmented_steering_angles.append(steering_angle * -1.0)
+    
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_steering_angles)
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Lambda, Conv2D, MaxPooling2D
@@ -32,7 +42,7 @@ model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 
 model.add(Conv2D(8, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Conv2D(8, 5, 5, activation='relu'))
+model.add(Conv2D(16, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
 
 model.add(Flatten())
@@ -41,7 +51,6 @@ model.add(Dense(64))
 model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
 
 model.save('model.h5')
-
