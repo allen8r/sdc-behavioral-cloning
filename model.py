d603705fd524d6744dc613c22666672c99add2f3
Allen C. Lau <allen.c.lau@gmail.com>
2017-11-01 19:16:36 -0700

    Update model to use LeNet architecture; add Conv2D with relu activation, MaxPooling2D, more Dense layers

--- model.py
+++ model.py
@@ -25,15 +25,23 @@ X_train = np.array(images)
 y_train = np.array(steering_angles)
 
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Lambda
+from keras.layers import Dense, Flatten, Lambda, Conv2D, MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+
+model.add(Conv2D(8, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(8, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+
 model.add(Flatten())
+model.add(Dense(128))
+model.add(Dense(64))
 model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 
 model.save('model.h5')
 
