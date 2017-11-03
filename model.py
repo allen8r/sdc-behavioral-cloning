350bdf921cd58d498425ca164e767e82042996a3
Allen C. Lau <allen.c.lau@gmail.com>
2017-11-02 18:09:18 -0700

    Add dropout layers. Increase steering correction for left and right camera views.

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ for row in rows:
         image = cv2.imread(IMG_PATH + img_file_name)
         images.append(image)
     
-    CORRECTION = 0.2
+    CORRECTION = 0.85
     steering_angle = float(row[3])
     steering_angle_left = steering_angle + CORRECTION
     steering_angle_right = steering_angle - CORRECTION
@@ -40,19 +40,29 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_steering_angles)
 
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Lambda, Conv2D, MaxPooling2D
+from keras.layers import Dense, Flatten, Lambda, Conv2D, MaxPooling2D, Dropout
 
+DROPOUT_RATE = 0.65
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 
 model.add(Conv2D(8, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
+model.add(Dropout(DROPOUT_RATE))
 model.add(Conv2D(16, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
+model.add(Dropout(DROPOUT_RATE))
+model.add(Conv2D(32, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(DROPOUT_RATE))
 
 model.add(Flatten())
 model.add(Dense(128))
+model.add(Dropout(DROPOUT_RATE))
 model.add(Dense(64))
+model.add(Dropout(DROPOUT_RATE))
+model.add(Dense(32))
+model.add(Dropout(DROPOUT_RATE))
 model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
