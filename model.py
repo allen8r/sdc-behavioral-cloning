eb0dadfb8ad58377b55d680fe812b66b8bde290d
Allen C. Lau <allen.c.lau@gmail.com>
2017-11-07 22:32:10 -0800

    Use Nvidia self-driving car architecture model. Update to only make steering angle correction for left and right camera images if the center camera image has a non-zero steering angle.

--- model.py
+++ model.py
@@ -3,9 +3,11 @@ import cv2
 import numpy as np
 import random
 import sklearn
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers import Dense, Flatten, Lambda, Conv2D, MaxPooling2D, Dropout, Cropping2D
+
 
 
 DATA_PATH = './data/'
@@ -13,16 +15,7 @@ IMG_PATH = DATA_PATH + 'IMG/'
 CAM_CENTER = 0
 CAM_LEFT = 1
 CAM_RIGHT = 2
-DATA_BALANCE = 450
-
-
-def get_img(cam_position, row_data):
-    '''
-    Retrieve the camera position image from the provided row data
-    '''
-    image = cv2.imread(IMG_PATH + row_data[cam_position].split('/')[-1])
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    return image
+DATA_BALANCE = 600
 
 
 # Process csv data
@@ -43,8 +36,16 @@ with open(DATA_PATH + 'driving_log.csv') as driving_data:
         elif float(line[3]) != 0.0:
             samples.append(line)
 
-
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+
+def get_img(cam_position, row_data):
+    '''
+    Retrieve the camera position image from the provided row data
+    '''
+    image = cv2.imread(IMG_PATH + row_data[cam_position].split('/')[-1])
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
 
 
 def batch_generator(samples, batch_size=32):
@@ -61,10 +62,13 @@ def batch_generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 images.extend([get_img(CAM_CENTER, batch_sample), get_img(CAM_LEFT, batch_sample), get_img(CAM_RIGHT, batch_sample)])
                 
-                CORRECTION = 0.65
+                CORRECTION = 0.3
                 steering_angle_center = float(batch_sample[3])
-                steering_angle_left = steering_angle_center + CORRECTION
-                steering_angle_right = steering_angle_center - CORRECTION
+                steering_angle_left = 0.0
+                steering_angle_right = 0.0
+                if steering_angle_center != 0.0:
+                    steering_angle_left = steering_angle_center + CORRECTION
+                    steering_angle_right = steering_angle_center - CORRECTION
                 steering_angles.extend([steering_angle_center, steering_angle_left, steering_angle_right])
 
             # Augment the data by horizontally flipping the images
@@ -82,24 +86,25 @@ def batch_generator(samples, batch_size=32):
             yield sklearn.utils.shuffle(X_batch, y_batch)
 
 
-# Build the model for training
+# Build the model for training; using NVIDIA self-driving car architecture
 DROPOUT_RATE = 0.50
 BATCH_SIZE = 64
 model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((65, 25), (0, 0))))
 
-model.add(Conv2D(6, (5, 5), activation='relu'))
-model.add(MaxPooling2D())
-model.add(Conv2D(6, (5, 5), activation='relu'))
-model.add(MaxPooling2D())
+model.add(Conv2D(24, (5, 5), strides=(2, 2), activation='relu'))
+model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
+model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 
 model.add(Flatten())
-model.add(Dense(128))
+model.add(Dense(100))
 model.add(Dropout(DROPOUT_RATE))
-model.add(Dense(64))
+model.add(Dense(50))
 model.add(Dropout(DROPOUT_RATE))
-model.add(Dense(32))
+model.add(Dense(10))
 model.add(Dropout(DROPOUT_RATE))
 model.add(Dense(1))
 
@@ -110,10 +115,23 @@ model.compile(optimizer='adam', loss='mse')
 train_generator = batch_generator(train_samples, batch_size=BATCH_SIZE)
 validation_generator = batch_generator(validation_samples, batch_size=BATCH_SIZE)
 
-model.fit_generator(train_generator,
-                    steps_per_epoch=len(train_samples) // BATCH_SIZE,
-                    validation_data=validation_generator,
-                    validation_steps=len(validation_samples) // BATCH_SIZE,
-                    epochs=10)
+model_fit = model.fit_generator(train_generator,
+                                        steps_per_epoch=len(train_samples) // BATCH_SIZE,
+                                        validation_data=validation_generator,
+                                        validation_steps=len(validation_samples) // BATCH_SIZE,
+                                        epochs=10)
 
 model.save('model.h5')
+
+
+# Visualize Mean Squared Error Loss
+print(model_fit.history.keys())
+
+plt.plot(model_fit.history['loss'])
+plt.plot(model_fit.history['val_loss'])
+plt.title('Model Mean Squared Error Loss')
+plt.ylabel('Mean Squared Error Loss')
+plt.xlabel('Epoch')
+plt.legend(['Training Set', 'Validation Set'], loc='upper right')
+plt.savefig('./visuals/loss_history.png')
+plt.show()
