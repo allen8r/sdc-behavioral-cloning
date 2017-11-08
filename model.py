8199af75a626622f44e3692c8bb406d6182b52d8
Allen C. Lau <allen.c.lau@gmail.com>
2017-11-07 17:28:53 -0800

    Add batch generator for retrieving data in batches. Add back some data frames with zero-steering angle to stabalize car on straigt aways.

--- model.py
+++ model.py
@@ -2,6 +2,10 @@ import csv
 import cv2
 import numpy as np
 import random
+import sklearn
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Lambda, Conv2D, MaxPooling2D, Dropout, Cropping2D
 
 
 DATA_PATH = './data/'
@@ -9,16 +13,20 @@ IMG_PATH = DATA_PATH + 'IMG/'
 CAM_CENTER = 0
 CAM_LEFT = 1
 CAM_RIGHT = 2
-DATA_BALANCE = 300
+DATA_BALANCE = 450
 
 
 def get_img(cam_position, row_data):
+    '''
+    Retrieve the camera position image from the provided row data
+    '''
     image = cv2.imread(IMG_PATH + row_data[cam_position].split('/')[-1])
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
+
 # Process csv data
-rows = []
+samples = []
 with open(DATA_PATH + 'driving_log.csv') as driving_data:
     reader = csv.reader(driving_data)
     next(reader, None) # skip header row
@@ -30,51 +38,60 @@ with open(DATA_PATH + 'driving_log.csv') as driving_data:
     zero_angle_count = 0
     for line in lines:
         if float(line[3]) == 0.0 and zero_angle_count < DATA_BALANCE:
-            rows.append(line)
+            samples.append(line)
             zero_angle_count += 1
         elif float(line[3]) != 0.0:
-            rows.append(line)
+            samples.append(line)
 
-# Extract images and steering angles
-images = []
-steering_angles = []
 
-for row in rows:
-    images.extend([get_img(CAM_CENTER, row), get_img(CAM_LEFT, row), get_img(CAM_RIGHT, row)])
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+
+def batch_generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:
+        random.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset + batch_size]
+
+            # Extract images and steering angles
+            images = []
+            steering_angles = []
+
+            for batch_sample in batch_samples:
+                images.extend([get_img(CAM_CENTER, batch_sample), get_img(CAM_LEFT, batch_sample), get_img(CAM_RIGHT, batch_sample)])
+                
+                CORRECTION = 0.65
+                steering_angle_center = float(batch_sample[3])
+                steering_angle_left = steering_angle_center + CORRECTION
+                steering_angle_right = steering_angle_center - CORRECTION
+                steering_angles.extend([steering_angle_center, steering_angle_left, steering_angle_right])
+
+            # Augment the data by horizontally flipping the images
+            augmented_images = []
+            augmented_steering_angles = []
+            for image, steering_angle in zip(images, steering_angles):
+                augmented_images.append(image)
+                augmented_images.append(cv2.flip(image, 1))
+                augmented_steering_angles.append(steering_angle)
+                augmented_steering_angles.append(steering_angle * -1.0)
     
-    CORRECTION = 0.75
-    steering_angle_center = float(row[3])
-    steering_angle_left = steering_angle_center + CORRECTION
-    steering_angle_right = steering_angle_center - CORRECTION
-    steering_angles.extend([steering_angle_center, steering_angle_left, steering_angle_right])
+            X_batch = np.array(augmented_images)
+            y_batch = np.array(augmented_steering_angles)
+            
+            yield sklearn.utils.shuffle(X_batch, y_batch)
 
-# Augment the data by horizontally flipping the images
-augmented_images = []
-augmented_steering_angles = []
-for image, steering_angle in zip(images, steering_angles):
-    augmented_images.append(image)
-    augmented_images.append(cv2.flip(image, 1))
-    augmented_steering_angles.append(steering_angle)
-    augmented_steering_angles.append(steering_angle * -1.0)
-    
-
-
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_steering_angles)
-print(len(X_train))
 
 # Build the model for training
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Lambda, Conv2D, MaxPooling2D, Dropout, Cropping2D
-
-DROPOUT_RATE = 0.55
+DROPOUT_RATE = 0.50
+BATCH_SIZE = 64
 model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((65, 25), (0, 0))))
 
-model.add(Conv2D(16, (5, 5), activation='relu'))
+model.add(Conv2D(6, (5, 5), activation='relu'))
 model.add(MaxPooling2D())
-model.add(Conv2D(32, (5, 5), activation='relu'))
+model.add(Conv2D(6, (5, 5), activation='relu'))
 model.add(MaxPooling2D())
 
 model.add(Flatten())
@@ -87,7 +104,16 @@ model.add(Dropout(DROPOUT_RATE))
 model.add(Dense(1))
 
 # Train the model
+# Adam optimizer with default parameters: lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
+
+train_generator = batch_generator(train_samples, batch_size=BATCH_SIZE)
+validation_generator = batch_generator(validation_samples, batch_size=BATCH_SIZE)
+
+model.fit_generator(train_generator,
+                    steps_per_epoch=len(train_samples) // BATCH_SIZE,
+                    validation_data=validation_generator,
+                    validation_steps=len(validation_samples) // BATCH_SIZE,
+                    epochs=10)
 
 model.save('model.h5')
